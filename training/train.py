3ecd2162d3ced2235f77ba59dc2cde70e1fa562f
Shriansh16 <shrianshsingh20@gmail.com>
2025-01-24 15:01:25 +0530

    updated

--- training/train.py
+++ training/train.py
@@ -28,18 +28,17 @@ def split_data(data_df):
 # Train the model, return the model
 def train_model(data, parameters):
     """Train a model with the given datasets and parameters"""
-    # The object returned by split_data is a tuple.
-    # Access train_data with data[0] and valid_data with data[1]
-
     train_data = data[0]
     valid_data = data[1]
 
-    model = lightgbm.train(parameters,
-                           train_data,
-                           valid_sets=valid_data,
-                           num_boost_round=500,
-                           early_stopping_rounds=20)
-
+    # valid_sets must be a list or tuple
+    model = lightgbm.train(
+        parameters,
+        train_data,
+        valid_sets=[valid_data],  # Wrap valid_data in a list
+        num_boost_round=500,
+        early_stopping_rounds=20
+    )
     return model
 
 
